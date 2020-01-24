66da3080066970ba07d47a42ef12fff8c8983e98
brnhaed <bernhard.grosser@gmail.com>
2020-01-24 22:00:49 +0100

    create upload folder if not exists, update gitignore

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,7 @@ from celery import Celery
 from flask_mail import Mail
 from flask_babel import Babel, lazy_gettext as _l
 from babel.core import negotiate_locale
+from pathlib import Path
 
 def patch_requests_class(app):
     reqclass = app.request_class
@@ -39,6 +40,8 @@ login.login_message = _l("Bitte logge dich ein.")
 celery = make_celery(app)
 celery.conf.update(app.config)
 mail = Mail(app)
+
+Path(app.config['IMAGE_UPLOAD_FOLDER']).mkdir(parents=True, exist_ok=True)
 
 from app.errors import bp as errors_bp
 app.register_blueprint(errors_bp)
