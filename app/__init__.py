391733324a5e5f25f2edce68e0bbd073aefa6c00
brnhaed <bernhard.grosser@gmail.com>
2020-03-09 21:32:38 +0100

    activate api

--- app/__init__.py
+++ app/__init__.py
@@ -46,8 +46,8 @@ mail = Mail(app)
 from app.errors import bp as errors_bp
 app.register_blueprint(errors_bp)
 
-#from app.api import bp as api_bp
-#app.register_blueprint(api_bp, url_prefix='/api/v1')
+from app.api import bp as api_bp
+app.register_blueprint(api_bp, url_prefix='/api/v1')
 from app.auth import bp as auth_bp
 app.register_blueprint(auth_bp, url_prefix='/auth')
 from app.ajax import bp as ajax_bp
